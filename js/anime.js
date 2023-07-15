640ce49d5e4a6393a7e93e5cb5bfdb7c9b3eb491
gonza <gonzaloriveros01@gmail.com>
2023-07-15 16:19:13 -0400

    Se agregan mapa anime

---
+++ js/anime.js
@@ -0,0 +1,57 @@
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.nav__anime', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+    sr.reveal('.titulo__cardC', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-50px'
+    });
+
+
+    sr.reveal('', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-50px'
+    });
+
+
+    sr.reveal('.cont__formulario', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-50px'
+    });
+
+
+    sr.reveal('.img__contact', {
+        duration: 4500,
+        rotate:{
+            x: 1,
+            y: 180
+        }
+    });
+
+    sr.reveal('.img__p', {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '-50px'
+    });
+
+
+    sr.reveal('.body__card', {
+        duration: 4000,
+        origin: 'left',
+        distance: '-50px'
+    });
+
+    sr.reveal('.icon__card', {
+        duration: 4000,
+        origin: 'right',
+        distance: '-50px'
+    });
